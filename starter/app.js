b6c5d037288ed21aaceede96649c670c1b4542af
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-05-23 19:13:55 +0530

    deletion operation

--- starter/app.js
+++ starter/app.js
@@ -2,25 +2,20 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
-require('dotenv').config()
+require('dotenv').config();
 
 // Middlewares
+app.use(express.static('./public'));
 app.use(express.json());
 
 // Routes
-app.get('/hello', (req, res) => {
-  res.send('Task Manager Application!');
-});
 
 app.use('/api/v1/tasks', tasks);
 
-// app.get('/api/v1/tasks') - get all the tasks
-// app.post('/api/v1/tasks') - create a new task
-// app.get('/api/v1/tasks/:id') - get a single task
-// app.patch('/api/v1/tasks/:id') - update task
-// app.delete('/api/v1/tasks/:id') - delete task
 // REST - representational state transfer
 // GUI
+
+app.use()
 
 const port = 3000;
 
--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -32,9 +32,19 @@ const createTask = async (req, res) => {
 };
 
 const updateTask = async (req, res) => {
-  
-    res.status(200).json({ });
-  
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}!` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(501).json({ msg: error });
+  }
 };
 
 const deleteTask = async (req, res) => {
